668374859f30c025aec62bb1124c9d95a097b9f2
Emma F. Mkpurunchi <87911811+Emmadegreat@users.noreply.github.com>
2021-08-24 00:19:31 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+$("h").css("color","brown");
+$("h").css("color","midnightblue");
+$("").attr("href","https://www:eesa5nalists.com.ng")
+$("h").on("mouseout", function(){
+	$("h").css("color","red");
+});
+/*$("button").on("click", function() {
+	$("h2").hide();
+});
+$("button").on("click", function() {
+	$("h2").toggle();
+});
+$("button").on("click", function() {
+	$("h2").toggle();
+});
+$("button").on("click", function() {
+	$("h2").slideUp();
+});
+$("button").on("click", function() {
+	$("h2").slideDown();
+});
+$("button").on("click", function() {
+	$("h2").slideToggle();
+});
+$("button").on("click", function() {
+	$("h2").animate({opacity:0.5});
+});
+$("button").on("click", function() {
+	$("h2").slideUp().slideDown();
+});
+$("h2").on("mouseover", function() {
+	$("h2").html("welcome");
+});
+$("h2").on("mouseout", function() {
+	$("h2").html("header two");
+});*/
+/*$(".first_cont").css("background-color","steelblue");
+$("#first_row").css("background-color","#fff");
+*/
+$("section").on("mouseover",function(){
+	$("#nf").html("LIVING THE FAITH !");
+});
+$("section").on("mouseout",function(){
+	$("#nf").html("N  F  C  S !");
+});
+$(".che").on("click",function(){
+	$("n").slideToggle();
+});
