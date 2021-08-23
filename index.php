96c35d661e43fc7b36ca5d57108203b85f464978
Emma F. Mkpurunchi <87911811+Emmadegreat@users.noreply.github.com>
2021-08-24 00:04:08 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,312 @@
+<?php ob_start(); ?>
+
+    <?php
+    session_start();
+    include "html/header1.html";
+//echo $_SESSION['surname'];
+    ?>
+    
+        
+     <section>
+       <p id="nf"><strong>N &nbsp;&nbsp;F&nbsp;&nbsp;C&nbsp;&nbsp;S!</strong></p>
+     </section>
+
+    
+    
+  <div id="myCarousel" class="carousel slide" data-ride="carousel" data-pause="hover" data-interval="3000">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img class="image-responsive" src="images/eesa555.jpg" alt="eesa555.jpg">
+      <div class="carousel-caption">
+      <strong style="font-size:18px">NFCS EXECUTIVES 2021</strong>
+      </div>
+    </div>
+
+    <div class="item" >
+      <img class="image-responsive" src="images/eesalogo.jpg" alt="eesa555.jpg">
+      <div class="carousel-caption">
+        <strong style="font-size:18px">NFCS CHAPLAIN, UNIZIK</strong>
+      </div>
+    </div>
+
+    <div class="item">
+      <img class="image-responsive" src="images/eesa555.jpg" alt="eesa555.jpg" >
+      <div class="carousel-caption">
+        <strong style="font-size:18px">NFCS CHAPLAINCY BUILDING UNIZIK</strong>
+      </div>
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+  <span class="fa fa-chevron-left" aria-hidden="true"></span>
+    <!--<span class="sr-only">Previous</span>-->
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="fa fa-chevron-right" aria-hidden="true"></span>
+    <!--<span class="sr-only">Next</span>-->
+  </a>
+</div>
+<!-------------------------OUR PROJECTS---------------------------->
+        <h1>OUR PROJECTS</h1>
+<div class="container-fluid" id="first_cont">
+  
+
+  <div class="row" id="first_row">
+    <div class="col-sm-4">
+      <h3>project one here</h3>
+    </div>
+
+     <div class="col-sm-4">
+      <h3>project two</h3>
+    </div>
+
+     <div class="col-sm-4">
+      <h3>project three</h3>
+      
+    </div>
+  </div>
+
+        <!--------------------------MEET OUR PASTORS-------------------------->
+
+  <div class="row">
+    <h1 class="h1">MEET OUR PASTORS</h1>
+
+    <div class="col-sm-4">
+          
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3><span>NFCS Chaplain UNIZIK</span></h3>
+        </div>
+        <div class="panel-body">
+          <img src="images/emfpix.jpg" class="image-responsive" alt="EMF pix" onmouseleave="pix()" id="profile_pix">
+        </div>
+        <div class="panel-footer">
+          <p>Rev.Fr. Remigius Ezeokonkwo</p>
+
+        </div>
+      </div>
+
+    </div>
+
+
+    <div class="col-sm-4">
+          
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3>Vicar</h3>
+        </div>
+        <div class="panel-body">
+          <img src="images/eesa555.jpg" class="image-responsive" alt="img1" id="profile_pix1">
+        </div>
+        <div class="panel-footer">
+          <p>Rev.Fr. Joshua Anenye</p>
+        </div>
+      </div>
+
+    </div>
+
+
+    <div class="col-sm-4">
+          
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3>Priest-in-Residence</h3>
+        </div>
+        <div class="panel-body">
+           <img src="images/emf.jpg" class="image-responsive" alt="img1" id="profile_pix2">
+        </div>
+        <div class="panel-footer">
+          <p>Rev.Fr.Jude Barnaby Ini-Abasi</p>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+
+  <div class="row">
+    <h1 class="h1_">UPDATES, NEWS AND ARTICLES</h1 class="h1_">
+
+    <div class="col-sm-3">
+          
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3>ARTICLE 1</h3>
+        </div>
+        <div class="panel-body">
+          <a href="article1.php"  target="_blank" id="p">...click here to read more about what is happening in this page. you will really like it</a>
+        </div>
+        <div class="panel-footer">
+          <p></p>
+        </div>
+      </div>
+
+    </div>
+
+
+    <div class="col-sm-3">
+          
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3>ARTICLE 2</h3>
+        </div>
+        <div class="panel-body">
+          <a href="article2.php" target="_blank" id="p1">Unizik nfcs induction...</a>
+        </div>
+        <div class="panel-footer">
+          <p></p>
+        </div>
+      </div>
+
+    </div>
+
+
+    <div class="col-sm-3">
+          
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3>ARTICLE 3</h3>
+        </div>
+        <div class="panel-body">
+          <a href="article3.php" id="p2" target="_blank">Auxillary Bishop of Awka visit to St.Joseph Catholic chapliancy, unizik</a>
+        </div>
+        <div class="panel-footer">
+          <p></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-sm-3">
+          
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3>ARTICLE 4</h3>
+        </div>
+        <div class="panel-body">
+         <a href="article4.php" id="p3" target="_blank">Our relationship with God by...</a>
+        </div>
+        <div class="panel-footer">
+          <p></p>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+  <div class="row" id="last_row">
+    <h1>OUR ACTIVITIES</h1>
+    <div class="col-sm-4">
+      <p>
+          <strong>St.Albert game</strong><br>
+          For over one month ago, there has been news spreading on different social media platform speculating that the Federal Government of
+          Nigeria has declared Monday the 7th of September 2020 as the official resumption date for the tertiary institutions across the country.
+          This news has been debunked and declared as false, as the Federal ministry of education has not made such pronouncement concerning
+          school resumption.<br>
+          It should be recalled that the state minister of education Prof Chukwuemeka Nwajiuba on monday 31st of August 2020 during the covid-19 national briefing said "concerning
+           the resumption of schools the process is still on discussion." We therefore call on our students to disregard any information of this kind now and
+           always visit this site and the likes for authenttic information.
+
+          
+        </p>
+    </div>
+
+     <div class="col-sm-4">
+      
+      <p id="ppp">
+          <strong>our harvst</strong><br>
+          For over one month ago, there has been news spreading on different social media platform speculating that the Federal Government of
+          Nigeria has declared Monday the 7th of September 2020 as the official resumption date for the tertiary institutions across the country.
+          This news has been debunked and declared as false, as the Federal ministry of education has not made such pronouncement concerning
+          school resumption.<br>
+          It should be recalled that the state minister of education Prof Chukwuemeka Nwajiuba on monday 31st of August 2020 during the covid-19 national briefing said "concerning
+           the resumption of schools the process is still on discussion." We therefore call on our students to disregard any information of this kind now and
+           always visit this site and the likes for authenttic information.
+
+        </p>
+    </div>
+
+      <div class="col-sm-4">
+      
+          <p id="ppp">
+            <strong>father's sendforth</strong><br>
+            For over one month ago, there has been news spreading on different social media platform speculating that the Federal Government of
+            Nigeria has declared Monday the 7th of September 2020 as the official resumption date for the tertiary institutions across the country.
+            This news has been debunked and declared as false, as the Federal ministry of education has not made such pronouncement concerning
+            school resumption.<br>
+            It should be recalled that the state minister of education Prof Chukwuemeka Nwajiuba on monday 31st of August 2020 during the covid-19 national briefing said "concerning
+           the resumption of schools the process is still on discussion." We therefore call on our students to disregard any information of this kind now and
+           always visit this site and the likes for authenttic information.
+
+          </p>
+      </div>
+
+  </div>
+  <div class="row" style="padding-bottom: 30px;">
+    
+  </div><br>
+
+</div>
+
+      
+
+
+
+
+              
+              
+
+         <?php
+
+              if (isset($_POST["send"])) {
+                $user_name = htmlentities(addslashes($_POST['fullname']));
+                $user_email = htmlentities(addslashes($_POST['user_email']));
+                $message = htmlentities(addslashes($_POST['message']));
+                $subject = htmlentities(addslashes($_POST['subject']));
+
+                if (empty($user_name) || empty($user_email) || empty($message) || empty($subject)) {
+                  echo 'fill the form !';
+                }
+                else{
+
+                  $receiver_email = 'mkpurunchiemma@gmail.com';
+
+                  if (mail($receiver_email, $subject, $message)) {
+
+                    echo 'successfully submitted';
+                  }
+                }
+                
+
+              }
+         ?>
+        </div>
+    </div>
+
+</div>
+    
+ </div>
+    </div>
+
+
+
+    
+
+
+        
+
+      </div>
+    </div>
+
+    <script type="text/javascript" src="js/main.js"></script>
+    <?php include "html/footer.html"; ?>
+
+    <?php ob_end_flush(); ?>
